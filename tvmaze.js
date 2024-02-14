d72cc76329f45c6be0cf0b9b54a4ded37834ccdb
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-02-14 12:05:43 -0800

    Worked on getShowsByTerm function

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,9 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const TVMAZE_URL = "http://api.tvmaze.com/";
+const MISSING_URL = "https://tinyurl.com/tv-missing";
+
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,21 +15,36 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const params = new URLSearchParams({q:term});
+  //fetch(`/resource!${term}`);
+  console.log(params);
+  const response = await fetch(`${TVMAZE_URL}search/shows?${params}`);
+  console.log(response);
+  const data = await response.json();
+  console.log(await data);
+  const filteredData = data.map(show => (
+    id: show.id,
+    name: show.name,
+    summary: show.summary,
+    image: show.image ? show.image : MISSING_URL
+  ));
+
+
 
   return [
     {
       id: 1767,
       name: "The Bletchley Circle",
       summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
            women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
            and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
+           unsolved murder she is met by skepticism from the police. She
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
@@ -48,9 +66,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
